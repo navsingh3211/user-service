de65ac4209b5bf63a196bde610108102b422f868
navsingh3211 <navsingh3211@gmail.com>
2026-01-06 00:22:39 +0530

    user-service first time push

---
+++ src/main/java/com/ecommerce/user/services/UserService.java
@@ -0,0 +1,81 @@
+package com.ecommerce.user.services;
+
+import com.ecommerce.user.dto.AddressDTO;
+import com.ecommerce.user.dto.UserRequest;
+import com.ecommerce.user.dto.UserResponse;
+import com.ecommerce.user.models.Address;
+import com.ecommerce.user.models.User;
+import com.ecommerce.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<UserResponse> fetchAllUsers(){
+        return userRepository.findAll().stream()
+                .map(this::mapToUserResponse)
+                .collect(Collectors.toList());
+    }
+    public void addUser(UserRequest userRequest){
+        User user = new User();
+        updateUserFromRequest(user,userRequest);
+        userRepository.save(user);
+    }
+
+    public Optional<UserResponse> fetchUser(Long id) {
+        return userRepository.findById(id)
+                .map(this::mapToUserResponse);
+    }
+    public boolean updateUser(Long id,UserRequest userRequest ){
+        return userRepository.findById(id)
+                .map(existingUser->{
+                    updateUserFromRequest(existingUser,userRequest);
+                    userRepository.save(existingUser);
+                    return true;
+                }).orElse(false);
+    }
+
+    private void updateUserFromRequest(User user, UserRequest userRequest) {
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getLastName());
+        user.setEmail(userRequest.getEmail());
+        user.setPhone(userRequest.getPhone());
+        if(userRequest.getAddress()!=null){
+            Address address = new Address();
+            address.setStreet(userRequest.getAddress().getStreet());
+            address.setCity(userRequest.getAddress().getCity());
+            address.setState(userRequest.getAddress().getState());
+            address.setCountry(userRequest.getAddress().getCountry());
+            address.setZipcode(userRequest.getAddress().getZipcode());
+            user.setAddress(address);
+        }
+    }
+
+    private UserResponse mapToUserResponse(User user){
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(String.valueOf(user.getId()));
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setPhone(user.getPhone());
+        userResponse.setRole(user.getRole());
+        if(user.getAddress()!=null){
+            AddressDTO addressDTO = new AddressDTO();
+            addressDTO.setStreet(user.getAddress().getStreet());
+            addressDTO.setCity(user.getAddress().getCity());
+            addressDTO.setState(user.getAddress().getState());
+            addressDTO.setCountry(user.getAddress().getCountry());
+            addressDTO.setZipcode(user.getAddress().getZipcode());
+            userResponse.setAddress(addressDTO);
+        }
+        return userResponse;
+    }
+}
